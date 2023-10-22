76b67cfa1a0340ad23e9b5ddf1245dcc43ad42c2
ahamed001 <ahamed.irfan001@gmail.com>
2023-10-22 12:20:20 +0530

    first commit

---
+++ src/Components/Content.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div>
+        <div className='px-12 py-5 font-bold text-4xl text-left'>
+            Store
+        </div>
+        <div>
+            <div className=' mx-12 my-5 h-44 px-8 pt-32 rounded-lg font-semibold text-2xl text-left text-white bg-gradient-to-r from-blue-500 to-blue-600'>
+                Meal Plan
+            </div>
+            <div className=' mx-12 my-5 h-44 px-8 pt-32 rounded-lg font-semibold text-2xl text-left text-white bg-gradient-to-r from-pink-600 to-pink-700'>
+                Training Plan
+            </div>
+            <div className=' mx-12 my-5 h-44 px-8 pt-32 rounded-lg font-semibold text-2xl text-left text-white bg-gradient-to-r from-blue-700 to-blue-800'>
+                Supplement Routine
+            </div>
+            <div className=' mx-12 my-5 h-44 px-8 pt-32 rounded-lg font-semibold text-2xl text-left text-white bg-gradient-to-r from-slate-700 to-slate-800'>
+                Track Progress
+            </div>
+            <div className=' mx-12 my-5 h-44 px-8 pt-32 rounded-lg font-semibold text-2xl text-left text-white bg-gradient-to-r from-green-600 to-green-700'>
+                Recipe Builder
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Content
